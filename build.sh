48de26e9aefac30e62331b6a7bd0de6def00e039
ArnaudAstree <ArnaudAstree>
2016-02-26 18:23:51 +0100

    Modification conf pour dev env

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ docker login --username=laloutre42 --password=Maximus42 --email=Laloutre42@gmail
 docker push laloutre42/docker-foodrecipes
 
 # Deploy in OpenShift local cluster
-oc deploy docker-foodrecipes --latest -n default
+#oc deploy docker-foodrecipes --latest -n default
 
 # Run foodrecipes-client
-#docker stop foodrecipes-client
-#docker rm foodrecipes-client
-#docker run --name foodrecipes-client -d -p 80:80 laloutre42/docker-foodrecipes
+docker stop foodrecipes-client
+docker rm foodrecipes-client
+docker run --name foodrecipes-client -d -p 81:80 laloutre42/docker-foodrecipes
 
 #docker login -u laloutre42 -e laloutre42@gmail.com -p 0ejbMI52S7Up-0d6NpLYlYabtDOJ4EZPBFrhi5znfGY 172.30.99.242:5000
